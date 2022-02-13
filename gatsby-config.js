6df30bd8b9f39d348a6c92454462b5c65d45e9f3
John Kiernan <jkiernan12@yahoo.com>
2022-02-13 12:05:11 -0500

    Add mobile styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,6 @@ module.exports = {
     resolve: 'gatsby-plugin-web-font-loader',
     options: {
       google: {
-        families: ['Praise']
+        families: ['Praise', 'Abril Fatface']
       }}}]
 };
