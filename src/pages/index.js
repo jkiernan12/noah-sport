13772fdcb326e68a41fdb326667b8ea5f7796e25
John Kiernan <jkiernan12@yahoo.com>
2022-02-13 11:29:04 -0500

    Make nav functional

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,27 +48,40 @@ const IndexPage = () => {
         <p>Vintage Euro Shop</p>
       </div>
         <nav>
+          <a href="#about-section">
           <button>About</button>
+          </a>
+          <a href="#work-section">
           <button>Work</button>
+          </a>
+          <a href="#contact-section">
           <button>Contact</button>
+          </a>
         </nav>
       </header>
       <section className="hero">
-        <h2>Specializing in VW and Porches</h2>
+        <h2>Put something good here</h2>
       </section>
       <section className="about" id="about-section">
         <img src={about} />
         <article>
           <h3>About</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum luctus turpis in aliquet. Morbi accumsan dui vitae ligula dignissim volutpat. Duis euismod nibh vel nisi rutrum dictum. Fusce sit amet nulla ligula. Morbi rutrum malesuada dolor ac rutrum. Duis euismod dapibus tortor ut lacinia. Curabitur sed nisl et turpis consectetur rhoncus ut at ex. Duis euismod blandit posuere. Donec id rhoncus neque. Suspendisse feugiat massa et hendrerit aliquet. Ut at tristique justo. Quisque finibus vel dolor et malesuada.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum luctus turpis in aliquet. Morbi accumsan dui vitae ligula dignissim volutpat. Duis euismod nibh vel nisi rutrum dictum. Fusce sit amet nulla ligula. Morbi rutrum malesuada dolor ac rutrum.</p>
         </article>
       </section>
-      <section className="work">
+      <section className="work" id="work-section">
       <article>
       <h3>Work</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum luctus turpis in aliquet. Morbi accumsan dui vitae ligula dignissim volutpat. Duis euismod nibh vel nisi rutrum dictum. Fusce sit amet nulla ligula. Morbi rutrum malesuada dolor ac rutrum. Duis euismod dapibus tortor ut lacinia. Curabitur sed nisl et turpis consectetur rhoncus ut at ex. Duis euismod blandit posuere. Donec id rhoncus neque. Suspendisse feugiat massa et hendrerit aliquet. Ut at tristique justo. Quisque finibus vel dolor et malesuada.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque elementum luctus turpis in aliquet. Morbi accumsan dui vitae ligula dignissim volutpat. Duis euismod nibh vel nisi rutrum dictum. Fusce sit amet nulla ligula. Morbi rutrum malesuada dolor ac rutrum.</p>
       </article>
         <ImageGallery className="slider" items={images} thumbnailPosition="right" />
+      </section>
+      <section className="contact" id="contact-section">
+      <article>
+        <h3>Contact</h3>
+        <p>If you have an old European car that needs anything from an oil change and check over to a mechanical rehabilitation to get back on the road send me an email and let’s make it happen!</p>
+        <p className="email"><a href="mailto:noahsportri@gmail.com">noahsportri@gmail.com</a></p>
+      </article>
       </section>
     </main>
   )
