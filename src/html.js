553ca299307eba3dfe70acab6bfd7dc67db1109f
John Kiernan <jkiernan12@yahoo.com>
2022-02-17 21:03:14 -0500

    Switch redirect script

--- src/html.js
+++ src/html.js
@@ -22,6 +22,17 @@ export default function HTML(props) {
           dangerouslySetInnerHTML={{ __html: props.body }}
         />
         {props.postBodyComponents}
+        <script>
+        if (window.netlifyIdentity) {
+          window.netlifyIdentity.on("init", user => {
+            if (!user) {
+              window.netlifyIdentity.on("login", () => {
+                document.location.href = "/admin/";
+              });
+            }
+          })
+        }
+      </script>
       </body>
     </html>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -68,17 +68,6 @@ const IndexPage = () => {
         <p>101 Dexter Rd, East Providence, RI 02914</p>
         </a>
       </footer>
-      <script>
-        if (window.netlifyIdentity) {
-          window.netlifyIdentity.on("init", user => {
-            if (!user) {
-              window.netlifyIdentity.on("login", () => {
-                document.location.href = "/admin/";
-              });
-            }
-          })
-        }
-      </script>
     </main>
   )
 }
